677d5bd0ad1b3e0b33862f99256e25f57090b7f8
Isaac Asiedu <asieduisaac5775@gmail.com>
2023-03-30 10:05:46 +0000

    imgs moved

---
+++ js/common/lazyload.js
@@ -0,0 +1,21 @@
+const images = document.querySelectorAll('img[loading="lazy"]');
+
+const options = {
+  rootMargin: '0px',
+  threshold: 0.1
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const img = entry.target;
+      img.src = img.dataset.src;
+      img.classList.remove('lazy');
+      observer.unobserve(img);
+    }
+  });
+}, options);
+
+images.forEach(img => {
+  observer.observe(img);
+});
