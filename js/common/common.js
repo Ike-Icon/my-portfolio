bc198cad7a64c4f3fcd4e3def0e97626156ab4dd
isaac asiedu <asieduisaac5775@gmail.com>
2025-01-30 03:20:05 +0000

    updated

--- js/common/common.js
+++ js/common/common.js
@@ -16,4 +16,9 @@ backToTopBtn.addEventListener('click', function (e) {
 
 
 
-
+// Skills page icons and design
+for (var items = document.querySelectorAll(".item"), i = 0; i < items.length; i++)
+  if (items[i].hasAttribute("data-color")) {
+    var color = items[i].getAttribute("data-color");
+    items[i].style.backgroundColor = "#" + color
+  }
